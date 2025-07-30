bdce94e6d6cbf6bc98769b715bfee5909f176ba1
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-07-30 10:32:58 +0100

    Searches publications for matches with engine name

--- update_web_pages.py
+++ update_web_pages.py
@@ -15,20 +15,35 @@ def update_main_page(games_df):
         fileout.write(script)
 
 
-def update_game_engine_page(engine):
+def update_game_engine_page(engine: pd.Series, publications: pd.DataFrame) -> int:
     if engine["PubMed game citations"] == "-":
-        return
+        return 0
 
     if int(engine["PubMed game citations"]) > 0:
         link_name = engine["Name"].replace(" ", "_")
+        this_paper = set([engine["Name"]])
+        contains_this_paper = this_paper.issubset
+        papers = publications[
+            publications["Game Engines - Search"].map(contains_this_paper)
+        ]
+
+        print("Found " + str(len(papers)) + " papers for " + engine["Name"])
         with open("index.html", "r") as filein:
             html = filein.read()
         with open("engines/" + link_name + ".html", "w") as fileout:
             fileout.write(html)
+        return len(papers)
+    return 0
 
 
 if __name__ == "__main__":
     games_df = pd.read_json("data/game_engine.db")
     update_main_page(games_df)
+
+    publications_df = pd.read_json("data/game_engine_papers.db")
+
+    count = 0
     for index, engine in games_df.iterrows():
-        update_game_engine_page(engine)
+        count += update_game_engine_page(engine, publications_df)
+
+    print("Got ", count, " matches for ", len(publications_df), " publications.")
